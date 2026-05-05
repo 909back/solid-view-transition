7c3f28595a51eb51679b97654ec3b07a79d4d7a2
909back <909back@naver.com>
2026-05-05 17:57:45 +0900

    chore: config cloudflare preset

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { solidStart } from "@solidjs/start/config";
 import path from "path";
 
 export default defineConfig({
-  plugins: [solidStart(), nitro()],
+  plugins: [solidStart(), nitro({ preset: "cloudflare_module" })],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
